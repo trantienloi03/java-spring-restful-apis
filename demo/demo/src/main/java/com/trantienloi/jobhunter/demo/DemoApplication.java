18bb9d18eea79045d017c65659f555584a5cb6f8
trantienloi <trantienloi412@gmail.com>
2025-03-26 10:14:54 +0700

    security configuration

--- demo/demo/src/main/java/com/trantienloi/jobhunter/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/trantienloi/jobhunter/demo/DemoApplication.java
@@ -3,11 +3,11 @@ package com.trantienloi.jobhunter.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-})
+@SpringBootApplication
+// @SpringBootApplication(exclude = {
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+// org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+// })
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/demo/src/main/java/com/trantienloi/jobhunter/demo/controller/UserController.java
+++ demo/demo/src/main/java/com/trantienloi/jobhunter/demo/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +23,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> createNewUser(@RequestBody User user) {
+        String hashPass = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPass);
         User loi = this.userService.handleCreateUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(loi);
     }
